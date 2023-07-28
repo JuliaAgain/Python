2c25367e8c911989ae650656b521ef6526855183
Julia <>
2023-07-28 11:17:59 +0300

    Finished hometask24

--- hometask24.py
+++ hometask24.py
@@ -1,12 +1,14 @@
 from random import randint as rd
 n = int(input("Введите число кустов: "))
-weight = []
+weight = [] # список с массами ягод на каждом кусте
 for i in range(n):
     weight.append(rd(1, 15))
-print(weight)
-max_summa = 0
-for i in range(n):
-    if max_summa < weight[i - 1] + weight[i] + weight[(i + 1) % n]:
-        max_summa = weight[i - 1] + weight[i] + weight[(i + 1) % n]
-print(max_summa)
+print(*weight)
+sum_of_3 = [] # cписок сумм кг для соседних 3 кустов
+sum_of_3.append(weight[0] + weight[1]+ weight[-1])
+sum_of_3.append(weight[0] + weight[-2]+ weight[-1])
+for i in range(1, n-1):
+    sum_of_3.append(weight[i-1] + weight[i]+ weight[i+1])
+
+print(max(sum_of_3))
 
