d7ec4800ec0555f9dc0edd8a2b5c0b0d705b2b25
Julia <>
2023-08-16 21:44:48 +0300

    Added task 47

---
+++ task47.py
@@ -0,0 +1,10 @@
+#Ввод:
+trasformation = lambda x: x
+values = [1, 23, 42, 'asdfg']
+transformed_values = list(map(trasformation, values))
+if values == transformed_values:
+    print('ok')
+else:
+    print('fail')
+
+
